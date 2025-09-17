73a0c38f395d0f5ccd11625da3f9b04ed94287d4
YOUSOL <imjamminii@gmail.com>
2025-09-17 23:00:01 +0900

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <img src="https://github.com/MiruHeon/chaja-project/blob/main/CHAJA-logo-white.png?raw=true" alt="CHAJA 로고 화이트" width="200"/>
+    <img
+      src="https://github.com/MiruHeon/chaja-project/blob/main/CHAJA-logo-white.png?raw=true"
+      alt="CHAJA 로고 화이트"
+      width={200}
+    />
   );
 }
